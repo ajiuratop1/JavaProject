796338576ceff6542ad64c0d480f769235999c78
ajiuratop1 <k450598@gmail.com>
2022-10-19 14:52:15 +0200

    Initial commit

---
+++ src/table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Questions (questionid serial PRIMARY KEY, text VARCHAR(30) NOT NULL);
+CREATE TABLE Answers (answerid serial PRIMARY KEY, text VARCHAR(30) NOT NULL);
+INSERT INTO Questions (text) VALUES ('1. Я люблю читать научно-техническую литературу');
+INSERT INTO Questions (text) VALUES ('2. У меня хороший аппетит');
+INSERT INTO Questions (text) VALUES ('3. По утрам я обычно встаю свежим и отдохнувшим');
+INSERT INTO Questions (text) VALUES ('4. Думаю, что мне понравилась бы работа библиотекаря');
+INSERT INTO Questions (text) VALUES ('5. Малейший шум меня будит');
+INSERT INTO Questions (text) VALUES ('15. Временами мне в голову приходят такие нехорошие мысли, что о них лучше не рассказывать');
+INSERT INTO Questions (text) VALUES ('15. Временами мне в голову приходят такие нехорошие мысли, что о них лучше не рассказывать');
